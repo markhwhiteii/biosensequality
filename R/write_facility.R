3db838460dfa75835848a330eda404073ba9798e
Mark White <markhwhiteii@gmail.com>
2017-10-26 11:36:26 -0500

    reverting back due to error

--- R/write_facility.R
+++ R/write_facility.R
@@ -1,28 +1,27 @@
 #' Write NSSP BioSense Platform Data Quality Summary Reports for One Facility
-#'
-#' @description
+#' 
+#' @description 
 #' This function is a lightweight version of the `write_reports` function. It will generate summary and example workbooks, but only for
 #' one specified facility. The first summary workbook shows percents and counts of nulls and invalids, while the examples workbook
 #' generates de tailed information on records and visits that are null or invalid.
-#'
+#' 
 #' @param username Your BioSense username, as a string. This is the same username you may use to log into RStudio or Adminer.
 #' @param password Your BioSense password, as a string. This is the same password you may use to log into RStudio or Adminer.
 #' @param table The table that you want to retrieve the data from, as a string.
 #' @param mft The MFT (master facilities table) from where the facility name will be retrieved, as a string.
 #' @param start The start date time that you wish to begin pulling data from, as a string.
 #' @param end The end data time that you wish to stop pulling data from, as a string.
-#' @param facility The C_Biosense_Facility_ID for the facility that you wish to generate and write the report for.
+#' @param facility The C_Biosense_Facility_ID for the facility that you wish to generate and write the report for.  
 #' @param directory The directory where you would like to write the reports to (i.e., "~/Documents/MyReports"), as a string.
 #' @param nexamples An integer number of examples you would like for each type of invalid or null field in the examples workbooks for each facility.
 #' This defaults to 0, which will not generate these example workbooks.
 #' @import dplyr
 #' @import tidyr
 #' @import openxlsx
-#' @import RODBC
 #' @importFrom stringr str_replace_all
 #' @export
 write_facility <- function(username, password, table, mft, start, end, facility, directory="", nexamples=0) {
-
+  
   # pull data
   channel <- odbcConnect("BioSense_Platform", paste0("BIOSENSE\\", username), password) # open channel
   data <- sqlQuery(
@@ -31,36 +30,32 @@ write_facility <- function(username, password, table, mft, start, end, facility,
   )
   name <- as.character(unlist(unname(c(sqlQuery(channel, paste0("SELECT Facility_Name FROM ", mft, " WHERE C_Biosense_Facility_ID = ", facility)))))) # get name from mft
   odbcCloseAll() # close connection
-
-  # get hl7 values
-  data("hl7_values", envir=environment())
-  hl7_values$Field <- as.character(hl7_values$Field)
-
+  
   # get facility-level state summary of required nulls
-  req_nulls <- get_req_nulls(data) %>%
-    select(-c(C_Biosense_Facility_ID)) %>%
-    gather(Field, Value, 2:ncol(.)) %>%
-    spread(Measure, Value) %>%
-    right_join(hl7_values, ., by = "Field")
+  req_nulls <- get_req_nulls(data) %>% 
+    select(-c(C_Biosense_Facility_ID)) %>% 
+    gather(Field, Value, 2:ncol(.)) %>% 
+    spread(Measure, Value)
   # get facility-level state summary of optional nulls
-  opt_nulls <- get_opt_nulls(data) %>%
-    select(-c(C_Biosense_Facility_ID)) %>%
-    gather(Field, Value, 2:ncol(.)) %>%
-    spread(Measure, Value) %>%
-    right_join(hl7_values, ., by = "Field")
+  opt_nulls <- get_opt_nulls(data) %>% 
+    select(-c(C_Biosense_Facility_ID)) %>% 
+    gather(Field, Value, 2:ncol(.)) %>% 
+    spread(Measure, Value)
   # get facility-level state summary of invalids
-  invalids <- get_all_invalids(data) %>%
-    select(-c(C_Biosense_Facility_ID)) %>%
-    gather(Field, Value, 2:ncol(.)) %>%
-    spread(Measure, Value) %>%
-    right_join(hl7_values, ., by = "Field")
-
+  invalids <- get_all_invalids(data) %>% 
+    select(-c(C_Biosense_Facility_ID)) %>% 
+    gather(Field, Value, 2:ncol(.)) %>% 
+    spread(Measure, Value)
+  
   # getting first and last visit date times
   vmin <- min(as.character(data$C_Visit_Date_Time))
   vmax <- max(as.character(data$C_Visit_Date_Time))
   amin <- min(as.character(data$Arrived_Date_Time))
   amax <- max(as.character(data$Arrived_Date_Time))
-
+  # get hl7 values
+  data("hl7_values", envir=environment())
+  hl7_values$Field <- as.character(hl7_values$Field)
+  
   # write to xlsx
   # initialize workbook
   wb <- createWorkbook()
@@ -68,18 +63,18 @@ write_facility <- function(username, password, table, mft, start, end, facility,
   sheet1 <- addWorksheet(wb, "Facility Information")
   writeDataTable(wb, sheet1,
                  suppressWarnings(data %>% # take data
-                                    select(c(C_Biosense_Facility_ID, Sending_Facility_ID, Sending_Application,
+                                    select(c(C_Biosense_Facility_ID, Sending_Facility_ID, Sending_Application, 
                                              Treating_Facility_ID, Receiving_Application, Receiving_Facility)) %>% # taking only variables we want
                                     gather(key=Field, value=Value, convert=TRUE) %>% # suppressed warnings because this will tell you it converted all to characters
                                     distinct() %>% # get only distinct entries
                                     bind_rows(data.frame(Field="Facility_Name", Value=name), .) %>% # add name to the top
                                     # bind with date ranges and number of records and visits
-                                    bind_rows(data.frame(Field=c("Patient_Visit_Dates", "Message_Arrival_Dates",
+                                    bind_rows(data.frame(Field=c("Patient_Visit_Dates", "Message_Arrival_Dates", 
                                                                  "Number of Records", "Number of Visits"),
                                                          Value=c(paste("From", vmin, "to", vmax),
                                                                  paste("From", amin, "to", amax),
-                                                                 nrow(data),
-                                                                 n_groups(group_by(data, C_BioSense_ID))))) %>%
+                                                                 nrow(data), 
+                                                                 n_groups(group_by(data, C_BioSense_ID))))) %>% 
                                     right_join(hl7_values, ., by="Field")), # get hl7 values
                  firstColumn=TRUE, bandedRows=TRUE)
   setColWidths(wb, sheet1, 1:3, "auto")
@@ -115,7 +110,7 @@ write_facility <- function(username, password, table, mft, start, end, facility,
                              death_invalid(data)[[1]], # 7
                              diagnosis_type_invalid(data)[[1]], # 8
                              discharge_disposition_invalid(data)[[1]], # 9
-                             ethnicity_invalid(data)[[1]], # 10
+                             ethnicity_invalid(data)[[1]], # 10 
                              facility_type_invalid(data)[[1]], # 11
                              fpid_mrn_invalid(data)[[1]], # 12
                              gender_invalid(data)[[1]], # 13
@@ -128,29 +123,29 @@ write_facility <- function(username, password, table, mft, start, end, facility,
                              temperature_invalid(data)[[1]], # 20
                              weight_invalid(data)[[1]], # 21
                              zip_invalid(data)[[1]]) # 22
-
+    
       inv_examples <- examples_invalids(facility, invalid_examples) # get examples of invalids from this facility
       null_examples <- examples_nulls(facility, data) # get examples of nulls from this faciltiy
       # join with other relevant fields
       inv_examples <- inv_examples %>% # take examples
-        left_join(., select(data, c(C_BioSense_ID, C_Visit_Date, C_Visit_Date_Time, First_Patient_ID,
-                                    C_Unique_Patient_ID, Medical_Record_Number, Visit_ID, Admit_Date_Time,
-                                    Recorded_Date_Time, Message_Date_Time, Create_Raw_Date_Time,
+        left_join(., select(data, c(C_BioSense_ID, C_Visit_Date, C_Visit_Date_Time, First_Patient_ID, 
+                                    C_Unique_Patient_ID, Medical_Record_Number, Visit_ID, Admit_Date_Time, 
+                                    Recorded_Date_Time, Message_Date_Time, Create_Raw_Date_Time, 
                                     Message_Type, Trigger_Event, Message_Structure, Message_Control_ID)),
                   by="C_BioSense_ID") %>% # join with all these fields, for every record of that visit
         rename(Invalid_Field=Field) %>% # make it clearer that that field is the one that is invalid
         group_by(Invalid_Field) %>% # group by type of field
         slice(1:nexamples) # get nexamples
       # do the same for nulls
-      null_examples <- null_examples %>%
-        left_join(., select(data, c(C_BioSense_ID, C_Visit_Date, C_Visit_Date_Time, First_Patient_ID,
-                                    C_Unique_Patient_ID, Medical_Record_Number, Visit_ID, Admit_Date_Time,
-                                    Recorded_Date_Time, Message_Date_Time, Create_Raw_Date_Time,
+      null_examples <- null_examples %>% 
+        left_join(., select(data, c(C_BioSense_ID, C_Visit_Date, C_Visit_Date_Time, First_Patient_ID, 
+                                    C_Unique_Patient_ID, Medical_Record_Number, Visit_ID, Admit_Date_Time, 
+                                    Recorded_Date_Time, Message_Date_Time, Create_Raw_Date_Time, 
                                     Message_Type, Trigger_Event, Message_Structure, Message_Control_ID)),
                   by="C_BioSense_ID") %>% # join with all these fields, for every record of that visit
         group_by(Null_Field) %>% # group by type of field
         slice(1:nexamples) # get nexamples
-
+      
       # write to excel workbook
       wb <- createWorkbook()
       # sheet 1: invalids
@@ -167,5 +162,5 @@ write_facility <- function(username, password, table, mft, start, end, facility,
       filename <- str_replace_all(name, "[^[a-zA-z\\s0-9]]", "") %>% # get rid of punctuation from faciltiy name
         str_replace_all("[\\s]", "_") # replace spaces with underscores
       saveWorkbook(wb, paste0(directory, "/", filename, "_Examples.xlsx"), overwrite=TRUE)
-  }
+  }  
 }
